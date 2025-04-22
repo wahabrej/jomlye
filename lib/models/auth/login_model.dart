8149a3ba835e2c223006b466a781e70cf70ba8d4
Wahid Murad <wahidmurad2000@gmail.com>
2025-04-22 12:16:40 +0600

    []common user data model created for User type data

--- lib/models/auth/login_model.dart
+++ lib/models/auth/login_model.dart
@@ -1,7 +1,9 @@
+import 'package:vidflix_flutter_app/models/common/common_user_model.dart';
+
 class SignInModel {
     final String? token;
     final String? type;
-    final User? user;
+    final CommonUserModel? user;
 
     SignInModel({
         this.token,
@@ -12,102 +14,6 @@ class SignInModel {
     factory SignInModel.fromJson(Map<String, dynamic> json) => SignInModel(
         token: json["token"],
         type: json["type"],
-        user: json["user"] == null ? null : User.fromJson(json["user"]),
-    );
-}
-
-class User {
-    final int? id;
-    final String? email;
-    final String? password;
-    final dynamic username;
-    final String? firstName;
-    final String? lastName;
-    final String? phone;
-    final dynamic image;
-    final dynamic dateOfBirth;
-    final String? userType;
-    final int? gender;
-    final dynamic token;
-    final String? theme;
-    final String? themeColor;
-    final dynamic joinDate;
-    final dynamic deactivateReason;
-    final dynamic firebaseAuthUid;
-    final String? permissions;
-    final dynamic interests;
-    final dynamic lastLogin;
-    final int? isUserBanned;
-    final int? isPasswordSet;
-    final dynamic socials;
-    final int? status;
-    final int? roleId;
-    final dynamic subscriptionId;
-    final dynamic rememberToken;
-    final DateTime? createdAt;
-    final DateTime? updatedAt;
-
-    User({
-        this.id,
-        this.email,
-        this.password,
-        this.username,
-        this.firstName,
-        this.lastName,
-        this.phone,
-        this.image,
-        this.dateOfBirth,
-        this.userType,
-        this.gender,
-        this.token,
-        this.theme,
-        this.themeColor,
-        this.joinDate,
-        this.deactivateReason,
-        this.firebaseAuthUid,
-        this.permissions,
-        this.interests,
-        this.lastLogin,
-        this.isUserBanned,
-        this.isPasswordSet,
-        this.socials,
-        this.status,
-        this.roleId,
-        this.subscriptionId,
-        this.rememberToken,
-        this.createdAt,
-        this.updatedAt,
-    });
-
-    factory User.fromJson(Map<String, dynamic> json) => User(
-        id: json["id"],
-        email: json["email"],
-        password: json["password"],
-        username: json["username"],
-        firstName: json["first_name"],
-        lastName: json["last_name"],
-        phone: json["phone"],
-        image: json["image"],
-        dateOfBirth: json["date_of_birth"],
-        userType: json["user_type"],
-        gender: json["gender"],
-        token: json["token"],
-        theme: json["theme"],
-        themeColor: json["theme_color"],
-        joinDate: json["join_date"],
-        deactivateReason: json["deactivate_reason"],
-        firebaseAuthUid: json["firebase_auth_uid"],
-        permissions: json["permissions"],
-        interests: json["interests"],
-        lastLogin: json["last_login"],
-        isUserBanned: json["is_user_banned"],
-        isPasswordSet: json["is_password_set"],
-        socials: json["socials"],
-        status: json["status"],
-        roleId: json["role_id"],
-        subscriptionId: json["subscription_id"],
-        rememberToken: json["remember_token"],
-        createdAt: json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
-        updatedAt: json["updated_at"] == null ? null : DateTime.parse(json["updated_at"]),
+        user: json["user"] == null ? null : CommonUserModel.fromJson(json["user"]),
     );
 }
