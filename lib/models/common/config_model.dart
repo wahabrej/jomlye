f02f11e2f5c163de5e4257df23529d570deb2f1a
Wahid Murad <wahidmurad2000@gmail.com>
2025-08-26 13:11:38 +0600

    blog details screen description issue fix

--- lib/models/common/config_model.dart
+++ lib/models/common/config_model.dart
@@ -1,14 +1,39 @@
 class ConfigModel {
     final String? currencySymbol;
     final String? googleDriveApiKey;
+    final dynamic bannerAds;
+    final dynamic bannerAdsId;
+    final dynamic interstitialAds;
+    final dynamic interstitialAdsId;
+    final dynamic admobPublisherId;
 
     ConfigModel({
         this.currencySymbol,
         this.googleDriveApiKey,
+        this.bannerAds,
+        this.bannerAdsId,
+        this.interstitialAds,
+        this.interstitialAdsId,
+        this.admobPublisherId,
     });
 
     factory ConfigModel.fromJson(Map<String, dynamic> json) => ConfigModel(
         currencySymbol: json["currency_symbol"],
         googleDriveApiKey: json["google_drive_api_key"],
+        bannerAds: json["banner_ads"],
+        bannerAdsId: json["banner_ads_id"],
+        interstitialAds: json["interstitial_ads"],
+        interstitialAdsId: json["interstitial_ads_id"],
+        admobPublisherId: json["admob_publisher_id"],
     );
+
+    Map<String, dynamic> toJson() => {
+        "currency_symbol": currencySymbol,
+        "google_drive_api_key": googleDriveApiKey,
+        "banner_ads": bannerAds,
+        "banner_ads_id": bannerAdsId,
+        "interstitial_ads": interstitialAds,
+        "interstitial_ads_id": interstitialAdsId,
+        "admob_publisher_id": admobPublisherId,
+    };
 }
